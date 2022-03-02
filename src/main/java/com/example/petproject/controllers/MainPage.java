68e9398ce73ffaa0fd167af38d90c572e1ff8eaf
biryuzabear <biryuzabear@gmail.com>
2022-03-02 21:45:16 +0300

    Сделана авторизация в вк, но хотелось бы как-то ее в отдельный сервис вынести

--- src/main/java/com/example/petproject/controllers/MainPage.java
+++ src/main/java/com/example/petproject/controllers/MainPage.java
@@ -1,15 +1,51 @@
 package com.example.petproject.controllers;
 
+import com.vk.api.sdk.client.TransportClient;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.UserActor;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.UserAuthResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainPage {
 
+    private static final Integer APP_ID = 8091762;
+    private static final String CLIENT_SECRET = "EDWu1sdfJzBuBre10ueC";
+    private static final String REDIRECT_URI = "http://localhost:8080/login";
+
+    TransportClient transportClient = new HttpTransportClient();
+    VkApiClient vk = new VkApiClient(transportClient);
+
+
     @GetMapping("/login")
-    public String mainPage(){
+    public String login(@RequestParam String code) throws ClientException, ApiException {
+
+        UserAuthResponse authResponse = vk.oAuth()
+                .userAuthorizationCodeFlow(APP_ID, CLIENT_SECRET, REDIRECT_URI, code)
+                .execute();
+
+        UserActor actor = new UserActor(authResponse.getUserId(), authResponse.getAccessToken());
+        System.out.println(authResponse.getAccessToken() + " " + authResponse.getExpiresIn());
         return "login";
     }
+
+    @GetMapping("/")
+    public String mainPage() {
+
+        return "redirect:https://oauth.vk.com/authorize" +
+                "?client_id=8091762" +
+                "&display=page" +
+                "&redirect_uri=http://localhost:8080/login" +
+                "&scope=ads" +
+                "&response_type=code" +
+                "&v=5.131";
+    }
+
 
 
 }
---
+++ src/main/java/com/example/petproject/services/VKService.java
@@ -0,0 +1,14 @@
+package com.example.petproject.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VKService {
+
+    public VKService() {
+
+
+
+
+    }
+}
